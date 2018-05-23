e59160e4551c1f08dadae8fcb74084a86bf4cad2
Andrew Byrd <andrew.byrd99@gmail.com>
2018-05-23 14:08:11 -0400

    set up goal theme and context

--- src/js/components/GoalBlock.js
+++ src/js/components/GoalBlock.js
@@ -4,98 +4,57 @@ import produce from 'immer';
 import { KeyGen } from '../util/KeyGen';
 import { ItemObjectCreator } from '../util/ItemObjectCreator';
 
+import { GoalProvider, GoalContext } from './theme/GoalTheme';
+
 import Block from './common/Block';
 import ItemCreator from './common/ItemCreator';
 import Container from './common/Container';
 import SubgoalBlock from './SubgoalBlock';
 
 class GoalBlock extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			creatorValue: '',
-			activeGoals: [],
-			completedGoals: [],
-		}
-	}
-
 	render() {
-		const {activeGoals, completedGoals, creatorValue} = this.state;
-
-		const cleanUpText = e => {
-			const newText = e.target.value.replace(/\n/g, '');
-			this.setState({ creatorValue: newText })
-		}
-
-		const createGoal = e => {
-			if (e.key === 'Enter') {
-				const value = e.target.value;
-				const uniqueKey = KeyGen('goal', this.state.activeGoals);
-				const goal = ItemObjectCreator('goal', value, uniqueKey);
-				this.setState(
-					produce(draft => {
-						draft.activeGoals.push(goal);
-						draft.creatorValue = '';
-					})
-				)
-			}
-		}
-
-		const goalChange = (keyToChange, value, goalKey) => {
-			this.setState(
-				produce(draft => {
-					draft.activeGoals.forEach((goal, index) => {
-						if (goal.key === goalKey) {
-							switch(keyToChange) {
-								case 'value':
-									const newText = value.replace(/\n/g, '');
-									if (value === '') {
-										goal.modalVisible = true;
-									} else {
-										goal[keyToChange] = newText;
-									}
-									break;
-								case 'delete':
-									draft.activeGoals.splice(index, 1);
-									break;
-								default:
-									goal[keyToChange] = value;
-							}
-						}
- 					});
-				})
-			)
-		}
-
 		return (
-			<Block type="goal">
-				<Container type="goal">
-					{
-						activeGoals.length > 0 && activeGoals.map(goal => {
-							return (
-								<SubgoalBlock
+			<GoalProvider>
+				<Block type="goal">
+					<Container type="goal">
+						<GoalContext.Consumer>
+							{
+								context => {
+									const { activeGoals } = context;
+									activeGoals.length > 0 && activeGoals.map(goal => {
+										return (
+											<SubgoalBlock
+												type="goal"
+												value={goal.value}
+												key={goal.key}
+												goalkey={goal.key}
+												isOpen={goal.isOpen}
+												isComplete={goal.isComplete}
+												modalVisible={goal.modalVisible}
+												goalChange={goalChange}
+											/>
+										)
+									})
+								}
+							}
+						</GoalContext.Consumer>
+					</Container>
+					<GoalContext.Consumer>
+						{
+							context => (
+								<ItemCreator
+									placeholder="Create a goal..."
 									type="goal"
-									value={goal.value}
-									key={goal.key}
-									goalkey={goal.key}
-									isOpen={goal.isOpen}
-									isComplete={goal.isComplete}
-									modalVisible={goal.modalVisible}
-									goalChange={goalChange}
+									value={context.creatorValue}
+									onChange={e => context.cleanUpText(e)}
+									onKeyPress={e => context.createGoal(e)}
+									defaultHeight={72}
 								/>
-							)
-						})
-					}
-				</Container>
-				<ItemCreator
-					placeholder="Create a goal..."
-					type="goal"
-					value={creatorValue}
-					onChange={e => cleanUpText(e)}
-					onKeyPress={e => createGoal(e)}
-					defaultHeight={72}
-				/>
-			</Block>
+							) 
+						}
+					</GoalContext.Consumer>
+				</Block>
+			</GoalProvider>
 		)
 	}
 }
---
+++ src/js/components/theme/GoalTheme.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import produce from 'immer';
+
+export const GoalContext = React.createContext('goal')
+
+export class GoalProvider extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			creatorValue: '',
+			activeGoals: [],
+			completedGoals: [],
+		}
+	}
+	render() {
+
+		const cleanUpText = e => {
+			const newText = e.target.value.replace(/\n/g, '');
+			this.setState({ creatorValue: newText })
+		}
+
+		const createGoal = e => {
+			if (e.key === 'Enter') {
+				const value = e.target.value;
+				const uniqueKey = KeyGen('goal', this.state.activeGoals);
+				const goal = ItemObjectCreator('goal', value, uniqueKey);
+				this.setState(
+					produce(draft => {
+						draft.activeGoals.push(goal);
+						draft.creatorValue = '';
+					})
+				)
+			}
+		}
+
+		const goalChange = (keyToChange, value, goalKey) => {
+			this.setState(
+				produce(draft => {
+					draft.activeGoals.forEach((goal, index) => {
+						if (goal.key === goalKey) {
+							switch(keyToChange) {
+								case 'value':
+									const newText = value.replace(/\n/g, '');
+									if (value === '') {
+										goal.modalVisible = true;
+									} else {
+										goal[keyToChange] = newText;
+									}
+									break;
+								case 'delete':
+									draft.activeGoals.splice(index, 1);
+									break;
+								default:
+									goal[keyToChange] = value;
+							}
+						}
+					 });
+				})
+			)
+		}
+
+		return (
+			<GoalContext.Provider value={{
+				creatorValue: this.state.creatorValue,
+				activeGoals: this.state.activeGoals,
+				completedGoals: this.state.completedGoals,
+				cleanUpText,
+				createGoal,
+				goalChange
+			}}>
+				{this.props.children}
+			</GoalContext.Provider>
+		)
+	}
+}
